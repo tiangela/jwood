442d997273b3c1288c2963725b6273b16265dcb2
tiangela <tas14@yandex.ru>
2018-02-01 22:17:31 +0300

    Доделала хэдер и меню навигации

--- js/script.js
+++ js/script.js
@@ -32,3 +32,19 @@ anchors.forEach(function(item) {
     }, animationTime / framesCount);
   });
 });
+
+// Меню навигации
+var navMain = document.querySelector(".menu");
+var navToggle = document.querySelector(".menu__toggle");
+
+navMain.classList.remove("menu--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("menu--closed")) {
+    navMain.classList.remove("menu--closed");
+    navMain.classList.add("menu--open");
+  } else {
+    navMain.classList.add("menu--closed");
+    navMain.classList.remove("menu--open");
+  }
+});
